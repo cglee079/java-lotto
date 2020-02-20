1a15288ff220d89c5d9ee16147bb5458fa3953a0
cglee079 <cglee079@zuminternet.com>
2020-02-20 13:34:39 +0900

    refactor : 상금 enum으로 분리

--- src/main/java/com/zuniorteam/lotto/core/LottoOffice.java
+++ src/main/java/com/zuniorteam/lotto/core/LottoOffice.java
@@ -9,22 +9,6 @@ import java.util.*;
 
 public class LottoOffice {
 
-    public static final Map<Integer, Long> PRIZE_MONEYS;
-
-    static {
-        final Map<Integer, Long> prizeMoneys = new HashMap<>();
-
-        prizeMoneys.put(0, 0L);
-        prizeMoneys.put(1, 0L);
-        prizeMoneys.put(2, 0L);
-        prizeMoneys.put(3, 5000L);
-        prizeMoneys.put(4, 50000L);
-        prizeMoneys.put(5, 1500000L);
-        prizeMoneys.put(6, 200000000000L);
-
-        PRIZE_MONEYS = Collections.unmodifiableMap(prizeMoneys);
-    }
-
     public LottoResult getLottoResult(LottoBuyer lottoBuyer, List<LottoNumber> winningNumbers) {
         validate(lottoBuyer);
 
@@ -35,7 +19,7 @@ public class LottoOffice {
 
         for (Integer matchCount : matchLottos.keySet()) {
             final Integer matchedLottoCount = matchLottos.get(matchCount);
-            final Long prize = PRIZE_MONEYS.get(matchCount);
+            final Long prize = Prize.ofByMatchCount(matchCount).getMoney();
 
             totalPrize += prize * matchedLottoCount;
             matchResults.add(new MatchResult(matchCount, prize, matchedLottoCount));
